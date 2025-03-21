3587215a1c680340c7b820606850d9c3eaa981b2
Makutu <brightdreamfuture6@gmail.com>
2025-03-21 10:57:58 +0000

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,14 @@ import { verifyToken } from "./lib/jwt";
 
 export default async function middleware(req: NextRequest) {
   const cookieValue = req.cookies.get("session")?.value;
-  const session = await verifyToken(cookieValue ?? "");
-  if (!cookieValue || !session.user) {
+  if (!cookieValue) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  const session = await verifyToken(cookieValue);
+  if (!session || !session.user) {
     return NextResponse.redirect(new URL("/", req.nextUrl));
   }
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-};
