361b15406aefdd0847c8137955fdd6e189071fd7
Makutu <brightdreamfuture6@gmail.com>
2025-03-21 10:41:18 +0000

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { verifyToken } from "./lib/jwt";
-
-const publicRoutes = ["/"];
 
 async function getSession() {
   const session = (await cookies()).get("session")?.value;
@@ -11,9 +8,8 @@ async function getSession() {
 }
 
 export default async function middleware(req: NextRequest) {
-  // 2. Check if the current route is protected or public
   const session = await getSession();
-  const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
+
   if (!session) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
@@ -21,7 +17,6 @@ export default async function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
-// Routes Middleware should not run on
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
