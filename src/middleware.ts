a302038ad31f547ac8559ab0e9d7d9e833f007ca
Makutu <brightdreamfuture6@gmail.com>
2025-03-24 14:37:21 +0000

    setup middleware

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { verifyToken } from "@/lib/jwt";
+
+const publicRoutes = ["/"];
+
+async function _verifySession() {
+  const session = (await cookies()).get("session");
+  if (!session) return null;
+  const sessionValue = await verifyToken(session.value);
+  return sessionValue;
+}
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const session = await _verifySession();
+  if (!isPublicRoute && !session?.user) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  if (
+    isPublicRoute &&
+    session?.user &&
+    !req.nextUrl.pathname.startsWith("/courses")
+  ) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+// Routes Middleware should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
