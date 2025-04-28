9195b812dcda31072a039f423ede44abdacaf69a
David Ojo <ojodavid115@gmail.com>
2025-04-28 16:27:13 +0000

    fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,10 +4,10 @@ import * as schema from "./schema";
 import { config } from "dotenv";
 config({ path: ".env" });
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.NEXT_PUBLIC_DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
 
 export const db = drizzle(sql, { schema });
