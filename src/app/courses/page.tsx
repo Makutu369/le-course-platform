8b31a4e6d2c390de3c0ab717b9b70d84742574bc
Makutu <brightdreamfuture6@gmail.com>
2025-04-29 10:44:07 +0000

    fix db error

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -3,11 +3,8 @@ import { getCourses } from "@/lib/queries/queries";
 
 import CourseCard from "../(components)/course-video-section/course-card";
 import { getSession } from "@/lib/session";
-import { checkAdditionalInfoAdded } from "@/lib/queries/auth";
-
 export default async function Page() {
   const [session, courses] = await Promise.all([getSession(), getCourses()]);
-  await checkAdditionalInfoAdded();
   return (
     <div className=" h-[cal(100vh-65px)] flex flex-col gap-y-5 px-6 sm:px-9 lg:px-12 w-full bg-background">
       <div className="w-full flex justify-between items-center"></div>
--- src/lib/queries/auth.ts
+++ src/lib/queries/auth.ts
@@ -2,71 +2,11 @@ import { db } from "@/db";
 import { megaCenters, users } from "@/db/schema";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { eq } from "drizzle-orm";
-import { getSession } from "../session";
-import { redirect } from "next/navigation";
 const SECRET_KEY = process.env.JWT_SECRET || "supersecretkey";
 
 export async function hashPassword(password: string) {
   const salt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, salt);
-}
-
-export async function createUser(
-  email: string,
-  password: string,
-  firstName: string,
-  lastName: string
-) {
-  const hashedPassword = await hashPassword(password);
-  return db
-    .insert(users)
-    .values({
-      email,
-      password: hashedPassword,
-      firstName,
-      lastName,
-      role: "user",
-    })
-    .returning();
-}
-
-export async function findUserByEmail(email: string) {
-  return db.select().from(users).where(eq(users.email, email)).limit(1);
-}
-
-export async function getAllMegaCenters() {
-  const centers = await db.select().from(megaCenters);
-
-  return centers;
-}
-
-export async function assignUserToMC(MegaCenterId: string) {
-  try {
-    const sessionData = await getSession();
-    const res = await db
-      .update(users)
-      .set({ megaCenter: MegaCenterId })
-      .where(eq(users.id, sessionData?.userId ?? ""));
-    return res;
-  } catch (error) {
-    console.error("Error assigning user to mega center:", error);
-    throw error;
-  }
-}
-
-export async function addUserPhone(phone: string) {
-  try {
-    const sessionData = await getSession();
-
-    await db
-      .update(users)
-      .set({ phone: phone })
-      .where(eq(users.id, sessionData?.userId ?? ""));
-  } catch (error) {
-    console.error("Error adding user phone:", error);
-    throw error;
-  }
 }
 
 export function generateToken(user: {
@@ -82,28 +22,4 @@ export async function comparePassword(
   hashedPassword: string
 ) {
   return bcrypt.compare(plainPassword, hashedPassword);
-}
-
-export async function checkAdditionalInfoAdded() {
-  const currentUser = await getSession();
-  if (!currentUser) return;
-
-  const user = await db.query.users.findFirst({
-    where: (users, { eq }) => eq(users.id, currentUser.userId),
-    columns: {
-      phone: true,
-      megaCenter: true,
-    },
-  });
-
-  if (!user) return { error: "unauthorised" };
-  const { megaCenter } = user;
-
-
-  if (!megaCenter) {
-    redirect("/student-info");
-  }
-
-  redirect("/courses");
-
 }
